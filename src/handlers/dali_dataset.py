39a567302d4bfcb883e56ed7908cf72c2efba687
Greeshma <greeshmaseetharaman@gmail.com>
2023-05-13 21:59:37 +0200

    Adding base files for handling Audio data and performing spectral features such as MFCC.
    None of them have been implemented yet at this stage.

--- src/handlers/dali_dataset.py
+++ src/handlers/dali_dataset.py
@@ -36,7 +36,9 @@ class DALIDataset():
     def get_data(self) -> Dict:
         logging.info("Getting the data_path")
         if self._data_path is not None:
-            return dali_code.get_the_DALI_dataset(self._data_path, skip=[], keep=[])
+            dali_dataset = dali_code.get_the_DALI_dataset(self._data_path, skip=[], keep=[])
+            logging.info(f"The DALI dataset has been downloaded")
+            return dali_dataset
         else:
             raise TypeError(f"Set the data_path for the location of the DALI datasets; data_path = {self._data_path}")
 
@@ -46,6 +48,7 @@ class DALIDataset():
             dali_info = dali_code.get_info(self._data_path + 'info/DALI_DATA_INFO.gz')
             dali_df = pd.DataFrame(dali_info)[1:]
             dali_df.columns = dali_info[0]
+            logging.info(f"The DALI dataset has {len(dali_info)} rows in it")
             return dali_df
         else:
             raise TypeError(f"Set the data_path for the location of the DALI datasets; data_path = {self._data_path}")
@@ -54,7 +57,8 @@ class DALIDataset():
         logging.info("Downloading audio from youtube URLs associated with the info file")
         if self._data_path is not None or self._file_path is not None:
             dali_info = self.get_info()
-            dali_code.get_audio(dali_info, path_audio, skip=[], keep=[])
+            logging.info(f"The DALI Audio download has {len(dali_info)} errors in it")
+            dali_code.get_audio(dali_info, self._file_path, skip=[], keep=[])
         else:
             raise TypeError(f"Set the data_path & file_path for the location of the DALI datasets; "
                             f"data_path = {self._data_path}, file_path = {self._file_path}")
--- src/main.py
+++ src/main.py
@@ -3,14 +3,10 @@ import logging
 
 logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     dali = DALIDataset(data_path="/Users/macbook/PycharmProjects/songsLyricsGenerator/data/DALI_v1.0/", file_path="/Users/macbook/PycharmProjects/songsLyricsGenerator/data/")
-    logging.info(f"Starting DALI Dataset download")
     dali_dataset = dali.get_data()
-    logging.info(f"The DALI dataset has been downloaded")
     dali_info = dali.get_info()
-    logging.info(f"The DALI dataset has {len(dali_info)} rows in it")
     dali_audio_errors = dali.get_audio()
-    logging.info(f"The DALI Audio download has {len(dali_info)} errors in it")
+
 
