3b9b161725bedf4452a1156192884733e41bed0a
Greeshma <greeshmaseetharaman@gmail.com>
2023-06-02 21:55:55 +0200

    adding constants file and DataConstants class.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
 from handlers.dali_dataset import  DALIDataset
+from mir_constants import DataConstants
 import logging
 
 logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(funcName)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 if __name__ == '__main__':
-    dali = DALIDataset(data_path="/Users/macbook/PycharmProjects/songsLyricsGenerator/data/DALI_v1.0/")
+    DALI_CONSTANTS = DataConstants(name = "Dali dataset",
+                               dataset_path = "/Users/macbook/PycharmProjects/songsLyricsGenerator/data/DALI_v1.0/",
+                               dataset_info_path = "/Users/macbook/PycharmProjects/songsLyricsGenerator/data/DALI_v1.0/info/dali_info.csv",
+                               dataset_metadata = "",
+                               dataset_cleaned_metadata = "")
+
+    dali = DALIDataset(data_path=DALI_CONSTANTS.dataset_path)
     dali_dataset = dali.get_data()
     print(dali_dataset)
     dali_info = dali.get_info()
-    dali_info.to_csv("/Users/macbook/PycharmProjects/songsLyricsGenerator/data/DALI_v1.0/info/dali_info.csv")
+    dali_info.to_csv(DALI_CONSTANTS.dataset_info_path)
     # print(dali_info)
     # dali_audio_errors = dali.download_audio()
 
