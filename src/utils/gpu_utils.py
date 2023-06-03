bbfda680a6a5263a6bd2f9258a78a01cf8e45ab6
Greeshma <greeshmaseetharaman@gmail.com>
2023-06-04 00:01:49 +0200

    GPU Utils

--- src/utils/gpu_utils.py
+++ src/utils/gpu_utils.py
@@ -1,13 +1,16 @@
-import os
+import subprocess
 import logging
 
 logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(funcName)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
 def is_gpu_available():
-    gpu_info = os.system("nvidia - smi")
-    gpu_info = '\n'.join(gpu_info)
-    if gpu_info.find('failed') >= 0:
-        logging.info('Not connected to a GPU')
-    else:
-        logging.info(gpu_info)
+    try:
+        gpu_info: str = subprocess.run('nvidia-smi', check=True, stdout=subprocess.PIPE).stdout
+        gpu_info = '\n'.join(gpu_info)
+        if gpu_info.find('failed') >= 0:
+            logging.info('Not connected to a GPU')
+        else:
+            logging.info(gpu_info)
+    except:
+        logging_info("GPU enabled device not found. Looks like it is not a Unix device")
