bc33ede0e6bf6c66d831c6974be79836f5485ceb
Greeshma <greeshmaseetharaman@gmail.com>
2023-06-03 23:47:27 +0200

    GPU Utils

--- src/utils/gpu_utils.py
+++ src/utils/gpu_utils.py
@@ -1,10 +1,13 @@
 import os
+import logging
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(funcName)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
 def gpu_info():
     gpu_info = os.system("nvidia - smi")
     gpu_info = '\n'.join(gpu_info)
     if gpu_info.find('failed') >= 0:
-        print('Not connected to a GPU')
+        logging.info('Not connected to a GPU')
     else:
-        print(gpu_info)
+        logging.info(gpu_info)
