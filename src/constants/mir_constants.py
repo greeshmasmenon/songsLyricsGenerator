64da53a20882b92b091c42fde67eebccf973e4bb
Greeshma Menon <greeshmaseetharaman@gmail.com>
2023-07-07 22:45:35 +0200

    Changes to make it run in HPC

--- src/constants/mir_constants.py
+++ src/constants/mir_constants.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 class DataConstants:
     """constants for the use within the Songs to Lyrics generation"""
     NAME: str = "Dali Dataset"
-    DATASET_PATH: str = "/Users/macbook/PycharmProjects/songsLyricsGenerator/data/DALI_v1.0/"
-    DATASET_INFO_PATH: str = "/Users/macbook/PycharmProjects/songsLyricsGenerator/data/DALI_v1.0/info/dali_info.csv"
+    DATASET_PATH: str = "/home/users/gmenon/workspace/songsLyricsGenerator/data/DALI_v1.0"
+    DATASET_INFO_PATH: str = "/home/users/gmenon/workspace/songsLyricsGenerator/data/DALI_v1.0/info/dali_info.csv"
     DATASET_METADATA: str = ""
     DATASET_CLEANED_METADATA: str = ""
 
