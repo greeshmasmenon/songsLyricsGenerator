8a9926ed747161c9063de0dd538d0b3aa3626002
Greeshma Menon <greeshmaseetharaman@gmail.com>
2023-07-08 17:31:15 +0200

    Move to HPC

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -14,11 +14,11 @@ def data_preparation(dali_dataset: DALIDataset) -> str:
     dali_dataset.get_dataset()
     dali_dataset.download_information()
     dali_audio_errors = dali_dataset.download_audio()
-    return "completed"
+    return f"completed with {len(dali_audio_errors)} errors}"
 
 
 if __name__ == "__main__":
-    dali = DALIDataset(data_path=MacDataConstants.DATASET_PATH,
-                       audio_file_path=MacDataConstants.AUDIO_FILE_PATH,
-                       info_path=MacDataConstants.DATASET_INFO_PATH)
+    dali = DALIDataset(data_path=HPCDataConstants.DATASET_PATH,
+                       audio_file_path=HPCDataConstants.AUDIO_FILE_PATH,
+                       info_path=HPCDataConstants.DATASET_INFO_PATH)
     data_preparation(dali)
