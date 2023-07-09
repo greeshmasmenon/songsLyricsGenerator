f27df4b61e28148ac9ada96764cda20b6fd904f8
Greeshma Menon <greeshmaseetharaman@gmail.com>
2023-07-09 14:31:10 +0200

    Adding notebooks and HPC scripts to git

---
+++ hpc/Jupyter_with_CPU.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH -J SLG
+#SBATCH -N 1
+#SBATCH -G 4
+#SBATCH --ntasks-per-node=1
+#SBATCH -c 2   # Cores assigned to each tasks
+#SBATCH --time=0-10:00:00
+#SBATCH -p gpu
+
+print_error_and_exit() { echo "***ERROR*** $*"; exit 1; }
+module purge || print_error_and_exit "No 'module' command"
+# Python 3.X by default (also on system)
+module load lang/Python
+source slg_env/bin/activate
+module load  vis/FFmpeg
+
+jupyter notebook --ip $(hostname -i) --no-browser  & pid=$!
+sleep 5s
+jupyter notebook list
+jupyter --paths
+jupyter kernelspec list
+echo "Enter this command on your laptop: ssh -p 8022 -NL 8888:$(hostname -i):8888 ${USER}@access-iris.uni.lu " > notebook.log
+wait $pid
